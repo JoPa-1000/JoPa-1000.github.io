cc0ec476cb2d045e58fceaba6143cd737ef10474
Johannes Passolt <63647970+JoPa-1000@users.noreply.github.com>
2024-11-24 17:51:13 +0100

    Delete V1 directory

--- V1/main.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById('f');
-const inputField = document.getElementById('input-1');
-const outputField = document.getElementById('output-t');
-outputField.value = inputField.value;
-
-form.addEventListener('input', () => {
-    console.log('.');
-    outputField.value = inputField.value;
-  });
